511c0cb61d2204d64e6364c0ac0645930edbc2c7
ManuelMagide <manumagideok@gmail.com>
2023-01-30 13:45:46 -0300

    Commit de ejercicio 1 y 2

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,23 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	// alert("ok");
+
+	// var edad;
+
+	let nombre;
+
+	let nombreCompleto; // Notacion Lower camel case
+
+	nombre = prompt('Ingrese un nombre')
+
+	
+	// nombre = 'Messi'; // Asignacion de string.
+
+	// nombre = 'Jose Luis'; // Asignacion destructiva.
+
+	// Mostrar por alert.
+	alert(nombre);
+
 }
 
