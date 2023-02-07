234efa6e75f7cb37df5346a3b39912813f6c77d3
ManuelMagide <manumagideok@gmail.com>
2023-02-07 12:03:02 -0300

    trabajo practico 4

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,22 +1,18 @@
 //Al ingresar una edad debemos informar si la persona es mayor de edad (mas de 18 años) o adolescente (entre 13 y 17 años) o niño (menor a 13 años).
-function mostrar()
-{
+function mostrar() {
 	let edad;
 
 	edad = document.getElementById('txtIdEdad').value;
 
-	if (edad >= 18)
-	{
-		alert ('Sos mayor de edad.');
-	} 
+	if (edad >= 18) {
+		alert('Sos mayor de edad.');
+	}
 	//else if ...
-		else
-	{ if(edad >= 13 && edad <= 17)
-		{
+	else {
+		if (edad >= 13 && edad <= 17) {
 			alert('Sos adolescente.');
 		}
-		else 
-		{
+		else {
 			alert('Sos un niño.');
 		}
 	}
--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -8,7 +8,107 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
-function CalcularPrecio () 
-{
- 	
+function CalcularPrecio() {
+    let cantidad;
+    let marca;
+    let precioCantidad;
+    let precio;
+    let descuento;
+    let ingresosBrutos;
+    let precioFinal;
+
+    marca = document.getElementById('Marca').value;
+    cantidad = document.getElementById('txtIdCantidad').value;
+
+    cantidad = parseInt(cantidad);
+
+    precioCantidad = cantidad * 35;
+
+    ingresosBrutos = precioCantidad * 10 / 100;
+
+    precioFinal = precioCantidad + ingresosBrutos;
+
+    if (cantidad >= 6) {
+
+        //precio = precioCantidad / 2;
+        descuento = 0.50;
+    }
+    else {
+        if (cantidad == 5 && marca == 'ArgentinaLuz') {
+
+            //precio = precioCantidad * 40 / 100;
+            descuento = 0.60;
+
+        } else {
+
+            if (cantidad == 5 && marca != 'Argentina luz') {
+
+                //precio = precioCantidad * 30 / 100;
+                descuento = 0.70;
+
+            } else {
+
+                if (cantidad == 4 && marca == 'ArgentinaLuz') {
+
+                    //precio = precioCantidad * 25 / 100;
+                    descuento = 0.75;
+
+                } else {
+                    if (cantidad == 4 && marca == 'FelipeLamparas') {
+
+                        descuento = 0.75;
+
+                    } else {
+
+                        if (cantidad == 4 && marca != 'ArgentinaLuz' && marca != 'FelipeLamparas') {
+
+                            //precio = precioCantidad * 20 / 100;
+                            descuento = 0.80;
+
+                        } else {
+
+                            if (cantidad == 3 && marca == 'ArgentinaLuz') {
+
+                                descuento = 0.85;
+
+                            } else {
+
+                                if (cantidad == 3 && marca == 'FelipeLamparas') {
+
+                                    descuento = 0.90;
+
+                                } else {
+
+                                    if (cantidad == 3 && marca != 'ArgetinaLuz' && marca != 'FelipeLamparas') {
+
+                                        descuento = 0.95;
+
+                                    } else {
+
+                                        descuento = 1;
+
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+                }
+
+            }
+        }
+    }
+
+    precio = precioCantidad * descuento;
+
+    document.getElementById('txtIdprecioDescuento').value = precio;
+
+    if (precio >= 120) {
+
+        precio = precioFinal;
+
+        alert('Usted pago ' + precio + ' de IIBB');
+    }
 }
+
+/* txtIdCantidad  txtIdprecioDescuento */
